8760605f6939c18bd2472e86c517f9d7a9e54f58
나황제 <na03266@livewalk.co.kr>
2024-02-05 18:00:25 +0900

    update array

---
+++ 6_array/1_problems_with_array_in_js.ts
@@ -0,0 +1,17 @@
+/**
+ * Problem with Array in JS
+ */
+const number =[1,'2',3,'4',5];
+
+let strings: string[]= ['1','2','3'];
+
+// strings.push(1)
+
+let stringsOrNumbersArray:(string| number)[]=[
+    1,
+    '2',
+    3,
+    '4',
+]
+
+
