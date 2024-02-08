47af7a9578e90a3ce6710c09f729d64e45328bb7
나황제 <na03266@livewalk.co.kr>
2024-02-08 11:27:02 +0900

    update export and import

---
+++ 15_import_and_export/1_import_defalut_2.ts
@@ -0,0 +1,21 @@
+/**
+ * ./ 현재위치
+ * ../상위폴더
+ */
+import example from "./1_export_defalut_1";
+
+// const iu = new IdolModel('아이유', 23)
+// console.log(iu);
+
+// console.log(IdolModel);
+
+// const cat: IdolModel = {
+//     name: '냥냥이',
+//     breed: '스코티시'
+// }
+
+const yuJin = new example.IdolModel('안유진', 23)
+
+
+console.log(yuJin);
+console.log(example.number);
---
+++ 15_import_and_export/2_import_2.ts
@@ -0,0 +1,14 @@
+import {IdolModel, ICat, rose, number} from './2_export_1'
+
+const iu = new IdolModel('아이유', 12);
+console.log(iu);
+
+console.log(rose);
+console.log(number);
+
+const cat: ICat ={
+    name: '냥냥이',
+    age: 6,
+}
+
+console.log(cat);
